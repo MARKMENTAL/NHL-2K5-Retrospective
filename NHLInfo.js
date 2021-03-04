87b45d1eae44c066f2dbd76713c9d7a77700f063
MARKMENTAL <marky611@gmail.com>
2021-03-04 14:35:27 -0500

    Update NHLInfo.js
    All Star Break Update: Detailed Stats

--- NHLInfo.js
+++ NHLInfo.js
@@ -18,19 +18,19 @@
 "use strict";
 
 
-var gameTitle = "ESPN NHL 2K5 Fantasy Season Results <br> Game Date: December 13, 2004";
+var gameTitle = "ESPN NHL 2K5 Fantasy Season Results <br>  All Star Break: February 12, 2005";
 
 var division = ["Atlantic Division", "Northeast Division", "Southeast Division", "Pacific Division",
     "Northwest Division", "Central Division"
 ];
 
 var team = [
-    ["New York Rangers", "New Jersey Devils", "New York Islanders", "Pittsburgh Penguins", "Philadelphia Flyers"],
-    ["Buffalo Sabres", "Montreal Canadiens", "Toronto Maple Leafs","Boston Bruins", "Ottawa Senators"],
-    ["Carolina Hurricanes", "Tampa Bay Lightning", "Washington Capitals","Florida Panthers", "Atlanta Thrashers"],
-    ["Los Angeles Kings","San Jose Sharks","Phoenix Coyotes","Anaheim Ducks", "Dallas Stars"],
-    ["Edmonton Oilers", "Vancouver Canucks", "Colorado Avalanche", "Minnesota Wild", "Calgary Flames"],
-    ["Nashville Predators", "Columbus Blue Jackets", "Chicago Blackhawks", "St. Louis Blues", "Detroit Red Wings"]
+    ["New Jersey Devils", "Pittsburgh Penguins", "New York Rangers", "Philadelphia Flyers", "New York Islanders"],
+    ["Ottawa Senators", "Boston Bruins", "Buffalo Sabres","Montreal Canadiens", "Toronto Maple Leafs"],
+    ["Florida Panthers", "Washington Capitals", "Carolina Hurricanes","Atlanta Thrashers", "Tampa Bay Lightning"],
+    ["Los Angeles Kings","San Jose Sharks","Phoenix Coyotes","Dallas Stars", "Anaheim Ducks"],
+    ["Vancouver Canucks", "Edmonton Oilers", "Calgary Flames", "Colorado Avalanche", "Minnesota Wild"],
+    ["Nashville Predators", "Chicago Blackhawks", "Detroit Red Wings", "Columbus Blue Jackets", "St. Louis Blues"]
 ];
 
 var conference = [
@@ -44,30 +44,50 @@ var conference = [
 
 var wins = [
 //Eastern start
-    [10, 10, 5, 5, 3],
-    [8, 5, 6, 6, 5],
-    [8, 7, 5, 5, 3],
+    [14, 13, 12, 10, 10],
+    [17, 15, 15, 11, 12],
+    [12, 12, 10, 9, 9],
 //Eastern end
 //Western start
-    [11, 8, 6, 4, 5],
-    [9, 7, 6, 6, 5],
-    [10, 7, 7, 6, 4],
+    [16, 14, 12, 12, 9],
+    [16, 16, 13, 9, 10],
+    [19, 13, 11, 11, 9],
 //Western end
 ];
 
 var losses = [
 //Eastern start
-    [4, 3, 6, 7, 5],
-    [3, 3, 3, 7, 6],
-    [5, 7, 4, 8, 9],
+    [14, 10, 9, 12, 11],
+    [8, 11, 8, 8, 10],
+    [10, 12, 14, 17, 15],
 //Eastern end
 //Western start
-    [3, 5, 10, 5, 12],
-    [5, 8, 8, 6, 6],
-    [2, 4, 7, 4, 5],
+    [8, 9, 13, 19, 11],
+    [10, 11, 13, 12, 16],
+    [3, 12, 8, 12, 8],
 //Western end
 ];
-
-var AFCProQB =["Brett Favre","Kansas City Chiefs"];
-var NFCProQB =["Peyton Manning","Philadelphia Eagles"];
-
+var ties = [
+//Eastern start
+    [0, 2, 2, 1, 2],
+    [0, 1, 0, 3, 2],
+    [0, 0, 0, 3, 2],
+//Eastern end
+//Western start
+    [4, 2, 0, 0, 4],
+    [2, 0, 0, 1, 0],
+    [1, 3, 4, 2, 5],
+//Western end
+];
+var overtimeL = [
+//Eastern start
+    [0, 0, 2, 4, 2],
+    [0, 1, 0, 3, 0],
+    [3, 3, 1, 0, 0],
+//Eastern end
+//Western start
+    [1, 2, 0, 0, 1],
+    [0, 1, 1, 4, 1],
+    [1, 1, 3, 1, 1],
+//Western end
+];
